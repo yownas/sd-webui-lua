d4b6fa3fa2a273a488d77447419594afb07f6f9e
Jonas Stare <yownas@gmail.com>
2023-04-21 22:11:23 +0200

    Add gallery_add & gallery_reset

--- scripts/sd-webui-lua.py
+++ scripts/sd-webui-lua.py
@@ -12,20 +12,28 @@ def filter_attribute_access(obj, attr_name, is_setting):
 
 L = lupa.LuaRuntime(register_eval=False, attribute_filter=filter_attribute_access)
 G = L.globals()
+LUA_output = ''
+LUA_gallery = []
 
 def lua_run(lua_code):
-    result, image = L.execute(lua_code)
-    return result, [image]
-
+    global LUA_output, LUA_gallery
+    result = L.execute(lua_code)
+    if result:
+        LUA_output = result
+    return LUA_output, LUA_gallery
 
 def lua_reset(lua_code):
-    global L, G
+    global L, G, LUA_output, LUA_gallery
     L = lupa.LuaRuntime(register_eval=False, attribute_filter=filter_attribute_access)
     G = L.globals()
+    LUA_output = ''
+    LUA_gallery = []
     # Setup python functions
     G.console = sd_lua_console
     G.process = sd_lua_process
-    return ""
+    G.gallery_add = sd_lua_gallery_add
+    G.gallery_reset = sd_lua_gallery_reset
+    return LUA_output, LUA_gallery
 
 # Functions for Lua
 
@@ -65,28 +73,21 @@ def sd_lua_process(prompt):
         override_settings=[],
     )
 
-    #p.scripts = modules.scripts.scripts_txt2img
-    #p.script_args = args
-
-    #processed = modules.scripts.scripts_txt2img.run(p, *args)
-
-    #if processed is None:
-
     processed = process_images(p)
 
     p.close()
 
     return processed.images[0]
 
-lua_reset('')
+def sd_lua_gallery_add(image):
+    global LUA_gallery
+    LUA_gallery.append(image)
 
-#lua_def = '''
-#function LOG(text)
-#   sd_lua_console
-#end
-#'''
-#
-#L.execute(lua_def)
+def sd_lua_gallery_reset():
+    global LUA_gallery
+    LUA_gallery = []
+
+lua_reset('')
 
 def add_tab():
     with gr.Blocks(analytics_enabled=False) as tab:
@@ -97,9 +98,7 @@ def add_tab():
             reset = gr.Button('Reset')
         with gr.Row():
             results = gr.Textbox(label="Output", show_label=True, lines=10)
-            gallery = gr.Gallery(label="Gallery")
-
-        # add reset button (re-create L)
+            gallery = gr.Gallery(label="Gallery").style(preview=True)
 
         run.click(lua_run, inputs=[lua_code], outputs=[results, gallery])
         reset.click(lua_reset, inputs=[lua_code], outputs=[results, gallery])
