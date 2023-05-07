ed33407dcbd67028c8e1805a8cd0936318c5bfe5
Jonas Stare <yownas@gmail.com>
2023-05-07 12:09:29 +0200

    Simple example

---
+++ examples/simple_output.lua
@@ -0,0 +1,16 @@
+-- Simple output examples - Yownas
+
+-- Clear Output box
+ui.clear()
+
+-- Add text to Output
+ui.out("Adding text to Output with ui.out()")
+
+-- These two have basically the same functionality
+print("Using print()")
+ui.console("Using ui.consol()")
+
+-- An optional return() will write to the Output box
+a = 1
+b = 2
+return(a + b)
